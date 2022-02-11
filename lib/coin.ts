51a99fc32a79338d261a23a0d2fd8821c6426566
LeeSeungjae00 <seungjae266789@gmail.com>
2022-02-11 22:12:16 +0900

    캔들 계산 모듈 추가

---
+++ lib/coin.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { server_url } from '../utils/api_option';
+
+
+export async function getCoinList() {
+        const options: object = {
+            method: "GET",
+            url: server_url + "/v1/market/all",
+        };
+        const res = await axios(options);
+        return res.data;
+}
+
+export async function getDayCandle(market: string, count: number) {
+    try {
+        const options: object = {
+            method: 'GET',
+            headers: { Accept: 'application/json' },
+            url: server_url + `/v1/candles/days/?market=${market}&count=${count}&convertingPriceUnit=KRW`
+        };
+        const res = await axios(options);
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
