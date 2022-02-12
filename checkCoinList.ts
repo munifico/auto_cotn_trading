625fa55fb9f5fb15a53e216b8abf8d3ad969333b
LeeSeungjae00 <seungjae266789@gmail.com>
2022-02-12 19:48:20 +0900

    db 스키마 조정 및 사용 값들 계산 후 적용

--- checkCoinList.ts
+++ checkCoinList.ts
@@ -3,32 +3,45 @@ import timer from './utils/timer'
 import { dbConnect, dbInit } from './utils/databases'
 
 
+const K = 0.6;
+
 async function main() {
     const conn = dbInit();
     dbConnect(conn);
 
 
-    const sql = `INSERT INTO coinAutoTrading.coinList (id, coinDate, coinMarket, k) VALUES (?, ?, ?, ?);`
-    let params : Array<string>;
+    const sql = `INSERT INTO coinAutoTrading.coinList (id, coinDate, coinMarket, volume, targetPrice, rangePer) VALUES (?, ?, ?, ?, ?, ?);`
+    let params: Array<string>;
 
     let coinList: Array<any> = await getCoinList();
     for (const coin of coinList) {
-        await timer(0.3);
+        await timer(0.25);
         let [, coinCandle] = await getDayCandle(coin.market, 2);
+
+
+        let volume = coinCandle.high_price - coinCandle.low_price;
+        let rangePer = volume / coinCandle.low_price;
+        let amnioticFluid = coinCandle.trade_price - coinCandle.opening_price > 0 ? true : false;
+        let targetPrice = coinCandle.trade_price + volume * K;
 
         params = [
             coinCandle.candle_date_time_kst + coinCandle.market,
             coinCandle.candle_date_time_kst,
             coinCandle.market,
-            coinCandle.trade_price - coinCandle.opening_price
+            volume,
+            targetPrice,
+            rangePer
         ]
 
-        conn.query(sql, params, (err) => {
-            if(err) console.log('query is not excuted. insert fail...\n' + err);
-            else console.log(`insert ${params.join(', ')} success`);
-        })
-
+        if (amnioticFluid) {
+            conn.query(sql, params, (err) => {
+                if (err) console.log('query is not excuted. insert fail...\n' + err);
+                else console.log(`insert ${params.join(', ')} success`);
+            })
+        }
     }
+
+    return;
 }
 
 main();
--- utils/timer.ts
+++ utils/timer.ts
@@ -7,4 +7,5 @@ const timer = function (time : number) {
 };
 
 
+
 export default timer;
