843d41cfd487a218a37cdd0fd9fa9657e0923e57
LeeSeungjae00 <seungjae266789@gmail.com>
2022-02-17 11:39:36 +0900

    api try catch 외부로 위임

--- api/coin.ts
+++ api/coin.ts
@@ -3,40 +3,28 @@ import upbitAPIClient from './upbitAPIClient';
 import { AxiosRequestConfig } from 'axios';
 
 export async function getCoinList() {
-    try {
-        const res = await upbitAPIClient.get(GET_COIN_LIST)
-        return res.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const res = await upbitAPIClient.get(GET_COIN_LIST)
+    return res.data;
 }
 
 export async function getDayCandle(market: string, count: number) {
-    try {
-        const config: AxiosRequestConfig = {
-            params: {
-                market,
-                count,
-                convertingPriceUnit : 'KRW'
-            },
-        };
-        const res = await upbitAPIClient.get(GET_DAY_CANDLE, config);
-        return res.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const config: AxiosRequestConfig = {
+        params: {
+            market,
+            count,
+            convertingPriceUnit: 'KRW'
+        },
+    };
+    const res = await upbitAPIClient.get(GET_DAY_CANDLE, config);
+    return res.data;
 }
 
 export async function getNowPrice(coinList: string[]) {
-    try {
-        const config: AxiosRequestConfig = {
-            params: {
-                markets : coinList.join("%2C%20")
-            },
-        };
-        const res = await upbitAPIClient.get(GET_NOW_PRICE, config);
-        return res.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const config: AxiosRequestConfig = {
+        params: {
+            markets: coinList.join("%2C%20")
+        },
+    };
+    const res = await upbitAPIClient.get(GET_NOW_PRICE, config);
+    return res.data;
 }
