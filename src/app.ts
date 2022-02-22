51a34f87064569a4bd6a6a04dcb0c3f1b9ee88e4
LeeSeungjae00 <seungjae266789@gmail.com>
2022-02-22 21:08:22 +0900

    [ADD] 현재  얼마나 올라야 사는지에 대한 API 추가

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import { TodayCoinList } from './../types/dbResposeType';
 import express from 'express';
 import CronJob from 'cron';
 import checkCoinList from './checkCoinList';
 import tradingCoin from './tradingCoin';
 import sellingCoin from './sellingCoin';
+import { getNowPrice } from '../api/coin';
 
 
 const app = express();
@@ -10,94 +12,94 @@ const app = express();
 let buyCoinName: string = '';
 let candidateCoinsBuy: any = [
     {
-      id: '2022-02-21T09:00:00BTC-STPT',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-STPT',
-      volume: 6.799999999999998e-7,
-      targetPrice: 0.000002808,
-      rangePer: 0.181395
+        id: '2022-02-21T09:00:00BTC-STPT',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-STPT',
+        volume: 6.799999999999998e-7,
+        targetPrice: 0.000002808,
+        rangePer: 0.181395
     },
     {
-      id: '2022-02-21T09:00:00BTC-MLK',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-MLK',
-      volume: 0.000003099999999999997,
-      targetPrice: 0.00002365,
-      rangePer: 0.093861
+        id: '2022-02-21T09:00:00BTC-MLK',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-MLK',
+        volume: 0.000003099999999999997,
+        targetPrice: 0.00002365,
+        rangePer: 0.093861
     },
     {
-      id: '2022-02-21T09:00:00KRW-STPT',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'KRW-STPT',
-      volume: 30,
-      targetPrice: 127,
-      rangePer: 0.0891089
+        id: '2022-02-21T09:00:00KRW-STPT',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'KRW-STPT',
+        volume: 30,
+        targetPrice: 127,
+        rangePer: 0.0891089
     },
     {
-      id: '2022-02-21T09:00:00BTC-STRK',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-STRK',
-      volume: 0.00037021,
-      targetPrice: 0.001403706,
-      rangePer: 0.0762996
+        id: '2022-02-21T09:00:00BTC-STRK',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-STRK',
+        volume: 0.00037021,
+        targetPrice: 0.001403706,
+        rangePer: 0.0762996
     },
     {
-      id: '2022-02-21T09:00:00BTC-LUNA',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-LUNA',
-      volume: 0.00014723999999999987,
-      targetPrice: 0.001436264,
-      rangePer: 0.0575219
+        id: '2022-02-21T09:00:00BTC-LUNA',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-LUNA',
+        volume: 0.00014723999999999987,
+        targetPrice: 0.001436264,
+        rangePer: 0.0575219
     },
     {
-      id: '2022-02-21T09:00:00BTC-DAI',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-DAI',
-      volume: 0.0000018399999999999995,
-      targetPrice: 0.000028064,
-      rangePer: 0.049922
+        id: '2022-02-21T09:00:00BTC-DAI',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-DAI',
+        volume: 0.0000018399999999999995,
+        targetPrice: 0.000028064,
+        rangePer: 0.049922
     },
     {
-      id: '2022-02-21T09:00:00BTC-TUSD',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-TUSD',
-      volume: 0.00000212,
-      targetPrice: 0.000028212,
-      rangePer: 0.0494208
+        id: '2022-02-21T09:00:00BTC-TUSD',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-TUSD',
+        volume: 0.00000212,
+        targetPrice: 0.000028212,
+        rangePer: 0.0494208
     },
     {
-      id: '2022-02-21T09:00:00KRW-LSK',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'KRW-LSK',
-      volume: 600,
-      targetPrice: 3250,
-      rangePer: 0.0491803
+        id: '2022-02-21T09:00:00KRW-LSK',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'KRW-LSK',
+        volume: 600,
+        targetPrice: 3250,
+        rangePer: 0.0491803
     },
     {
-      id: '2022-02-21T09:00:00BTC-USDP',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-USDP',
-      volume: 0.000002180000000000003,
-      targetPrice: 0.000027588,
-      rangePer: 0.037431
+        id: '2022-02-21T09:00:00BTC-USDP',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-USDP',
+        volume: 0.000002180000000000003,
+        targetPrice: 0.000027588,
+        rangePer: 0.037431
     },
     {
-      id: '2022-02-21T09:00:00BTC-HBD',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'BTC-HBD',
-      volume: 0.00000159,
-      targetPrice: 0.000026754,
-      rangePer: 0.0344828
+        id: '2022-02-21T09:00:00BTC-HBD',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'BTC-HBD',
+        volume: 0.00000159,
+        targetPrice: 0.000026754,
+        rangePer: 0.0344828
     },
     {
-      id: '2022-02-21T09:00:00KRW-STRK',
-      coinDate: '2022-02-21T00:00:00.000Z',
-      coinMarket: 'KRW-STRK',
-      volume: 16940,
-      targetPrice: 62074,
-      rangePer: 0.033969
+        id: '2022-02-21T09:00:00KRW-STRK',
+        coinDate: '2022-02-21T00:00:00.000Z',
+        coinMarket: 'KRW-STRK',
+        volume: 16940,
+        targetPrice: 62074,
+        rangePer: 0.033969
     }
-  ];
+];
 
 let checkCoinListJob = new CronJob.CronJob('0 10 20 * * *', async () => {
     try {
@@ -128,6 +130,26 @@ app.get('/todayCoinList', (req, res) => {
 app.get('/buyCoin', (req, res) => {
     res.send(buyCoinName);
     console.log('buyCoin 호출');
+});
+
+app.get('/coinList/:market', async (req, res) => {
+    try {
+        const market = req.params.market;
+        req.body
+        const coinInfo = candidateCoinsBuy.find((coin: TodayCoinList) => coin.coinMarket === market);
+
+        const [nowPrice] = await getNowPrice([market]);
+        
+        const moneyRise = coinInfo.targetPrice - nowPrice.trade_price;
+        const perRise = moneyRise / nowPrice.trade_price * 100;
+        res.send({
+            moneyRise,
+            perRise
+        })
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 })
 
 
---
+++ types/dbResposeType.ts
@@ -0,0 +1,8 @@
+export interface TodayCoinList {
+    id: string;
+    coinDate: Date;
+    coinMarket: string;
+    volume: number;
+    targetPrice: number;
+    rangePer: number;
+}
